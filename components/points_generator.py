6c57e7cef7e1ef89b127aa4e3bc47b5e3b7a831b
SzymonStolarski <szymonpiotrstolarski@gmail.com>
2022-03-27 15:14:50 +0200

    Minor developments in PointsGenerator

--- components/points_generator.py
+++ components/points_generator.py
@@ -1,8 +1,6 @@
 import random
 from math import sqrt
 
-from models.RoutePoint import RoutePoint
-
 
 class PointsGenerator:
     COORDINATES_RANGE = (100, 200)
@@ -11,33 +9,55 @@ class PointsGenerator:
 
     def __init__(self, n_points: int) -> None:
         self.__n_points = n_points
-        self.__generated_points = list()
-        self.__distances = dict()
+        self.__generated_points = {}
+        self.__distances = {}
 
     def generate_points(self):
         for i in range(0, self.__n_points):
-            rndCoordX = random.randint(self.COORDINATES_RANGE[0], self.COORDINATES_RANGE[1])
-            rndCoordY = random.randint(self.COORDINATES_RANGE[0], self.COORDINATES_RANGE[1])
-            rndDemand = random.randint(self.DEMAND_RANGE[0], self.DEMAND_RANGE[1])
-            rndSupply = random.randint(self.SUPPLY_RANGE[0], self.SUPPLY_RANGE[1])
+            rnd_coord_x = random.randint(self.COORDINATES_RANGE[0],
+                                         self.COORDINATES_RANGE[1])
+            rnd_coord_y = random.randint(self.COORDINATES_RANGE[0],
+                                         self.COORDINATES_RANGE[1])
 
-            self.__generated_points.append(RoutePoint((rndCoordX, rndCoordY), rndDemand, rndSupply))
+            if random.getrandbits(1):
+                rnd_demand = random.randint(self.DEMAND_RANGE[0],
+                                            self.DEMAND_RANGE[1])
+                rnd_supply = 0
+            else:
+                rnd_demand = 0
+                rnd_supply = random.randint(self.SUPPLY_RANGE[0],
+                                            self.SUPPLY_RANGE[1])
 
-        self.__distances = self.__calculate_distance(self.__generated_points)
+            self.__generated_points[i] = {'coords': (rnd_coord_x, rnd_coord_y),
+                                          'demand': rnd_demand,
+                                          'supply': rnd_supply}
+        self.__generated_points[0]['demand'] = 0
+        self.__generated_points[0]['supply'] = 0
+        self.__distances = self.__calculate_distances(self.__generated_points)
 
         return self
-        pass
 
-    def __calculate_distance(self, points: list) -> dict:
-        distances = dict()
-        for i, point_x in enumerate(points):
-            for j, point_y in enumerate(points):
-                distance = 0.0
-                if i != j:
-                    distance = sqrt(
-                        pow((point_x.coords[0] - point_y.coords[0]), 2)
-                        + pow((point_x.coords[1] - point_y.coords[1]), 2))
+    def __calculate_distances(self, points: dict) -> dict:
+
+        dict_of_coords = {}
+        for k, val in points.items():
+            dict_of_coords[k] = val['coords']
+
+        distances = {}
+        for i, _ in enumerate(dict_of_coords):
+            for j, _ in enumerate(dict_of_coords):
+                distance = sqrt(
+                    pow((dict_of_coords[i][0] - dict_of_coords[j][0]), 2)
+                    + pow((dict_of_coords[i][1] - dict_of_coords[j][1]), 2))
+
                 distances[(i, j)] = distance
 
         return distances
-        pass
+
+    @property
+    def generated_points(self):
+        return self.__generated_points
+
+    @property
+    def distances(self):
+        return self.__distances
